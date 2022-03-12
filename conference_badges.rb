5bd97a0472be1502a37a1767ba0b2148911d13ea
katherineyuneman <kyuneman@gmail.com>
2022-03-11 21:16:48 -0500

    commit tree 46156006932bf3602dd61e02f8ca8936373d54ab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_names)
+    array_names.map do | name |
+        "Hello, my name is #{name}."
+    end
+end
+
+puts def assign_rooms(attendees)
+    room_assignments = []
+    x=0
+    while x < 7
+        attendees.each_with_index do |attendee, index|
+            x += 1
+            room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+            puts room_assignments
+        end
+    end
+end
+
